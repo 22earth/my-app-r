6bc190085653bf7662e789eb90d864841d3c4f4a
22earth <zhifengle@gmail.com>
2019-06-16 16:49:21 +0800

    add router demo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { renderRoutes } from 'react-router-config';
 import './App.less';
+// import AppRouter from './router-demo/app-router';
+// import Topic from './router-demo/topic';
+import routes from './router-demo/router';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-          demo
-      </header>
+      <Router>
+        {/* kick it all off with the root route */}
+        {renderRoutes(routes)}
+      </Router>
+      {/* <header className="App-header">demo</header>
+      <section>
+        <AppRouter />
+        <Topic />
+      </section> */}
     </div>
   );
 }
---
+++ src/router-demo/router.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { renderRoutes } from "react-router-config";
+
+const Root = ({ route }) => (
+  <div>
+    <h1>Root</h1>
+    {/* child routes won't render without this */}
+    {renderRoutes(route.routes)}
+  </div>
+);
+
+const Home = ({ route }) => (
+  <div>
+    <h2>Home</h2>
+  </div>
+);
+
+const Child = ({ route }) => (
+  <div>
+    <h2>Child</h2>
+    {/* child routes won't render without this */}
+    {renderRoutes(route.routes, { someProp: 'these extra props are optional' })}
+  </div>
+);
+
+const GrandChild = ({ someProp }) => (
+  <div>
+    <h3>Grand Child</h3>
+    <div>{someProp}</div>
+  </div>
+);
+
+const routes = [
+  {
+    component: Root,
+    routes: [
+      {
+        path: '/',
+        exact: true,
+        component: Home
+      },
+      {
+        path: '/child/:id',
+        component: Child,
+        routes: [
+          {
+            path: '/child/:id/grand-child',
+            component: GrandChild
+          }
+        ]
+      }
+    ]
+  }
+];
+
+export default routes;
