48a6e16d343bd6d02ed9c91e117c2dd24bd7ba94
22earth <zhifengle@gmail.com>
2020-10-13 09:41:31 +0800

    update: dependencies and proxy config

--- config-overrides.js
+++ config-overrides.js
@@ -5,23 +5,7 @@ module.exports = {
   webpack: override(
     addLessLoader({}),
     addWebpackAlias({
-      '@': path.join(__dirname, '.', 'src')
+      '@': path.join(__dirname, '.', 'src'),
     })
   ),
-  // https://github.com/timarney/react-app-rewired#extended-configuration-options
-  devServer: function(configFunction) {
-    return function(proxy, allowedHost) {
-      console.log('default: proxy', proxy);
-      const config = configFunction(
-        {
-          'xxx-app': {
-            changeOrigin: true,
-            target: 'http://localhost:7080'
-          }
-        },
-        allowedHost
-      );
-      return config;
-    };
-  }
 };
