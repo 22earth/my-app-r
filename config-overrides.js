e14027256e3156070f9962e172142b47a2a7b41e
22earth <zhifengle@gmail.com>
2021-04-22 16:10:01 +0800

    upgrade antd

--- config-overrides.js
+++ config-overrides.js
@@ -1,39 +1,30 @@
+const {
+  override,
+  fixBabelImports,
+  addLessLoader,
+  addWebpackAlias,
+} = require('customize-cra');
 const path = require('path');
-const { override, addLessLoader, addWebpackAlias,
-  fixBabelImports } = require('customize-cra');
-
-module.exports = {
-  webpack: override(
-    // npm install babel-plugin-import --save-dev
-    // https://github.com/ant-design/create-react-app-antd
-    // useBabelRc(),  // not work 2019-06-20
-    fixBabelImports('import', {
-      libraryName: 'antd',
-      libraryDirectory: 'es',
-      style: true,
-    }),
-    addLessLoader({
+module.exports = override(
+  fixBabelImports('import', {
+    libraryName: 'antd',
+    libraryDirectory: 'es',
+    style: true,
+  }),
+  addLessLoader({
+    lessOptions: {
       javascriptEnabled: true,
-      // modifyVars: { '@primary-color': '#1DA57A' }
-    }),
-    addWebpackAlias({
-      '@': path.join(__dirname, '.', 'src')
-    })
-  ),
-  // https://github.com/timarney/react-app-rewired#extended-configuration-options
-  devServer: function(configFunction) {
-    return function(proxy, allowedHost) {
-      console.log('default: proxy', proxy);
-      const config = configFunction(
-        {
-          'xxx-app': {
-            changeOrigin: true,
-            target: 'http://localhost:7080'
-          }
-        },
-        allowedHost
-      );
-      return config;
-    };
-  }
-};
+      modifyVars: { '@primary-color': '#2082ED' },
+    },
+  }),
+  // addLessLoader({
+  //   javascriptEnabled: true,
+  //   modifyVars: {
+  //     '@primary-color': '#2082ED',
+  //   },
+  // }),
+  addWebpackAlias({
+    '@': path.resolve(__dirname, 'src'),
+    components: path.resolve(__dirname, 'src/components'),
+  })
+);
